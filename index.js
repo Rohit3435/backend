3a53bc181b76450f85c84d6872aa94d9e58e54cf
Rohit3435 <rohitverma343536@gmail.com>
2026-01-07 11:59:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// const express = require('express')
+// const app = express()
+// const port = 4000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// //defined by rohit
+// app.get('/insta',(req,res)=>{
+//     res.send('How are you?')
+// })
+
+
+
+// //similarly this one is defined by using one of the property or functionalities of express and ultimately callback is used
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+//using dotenv
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//defined by rohit
+app.get('/insta',(req,res)=>{
+    res.send('How are you?')
+})
+
+
+
+//similarly this one is defined by using one of the property or functionalities of express and ultimately callback is used
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
